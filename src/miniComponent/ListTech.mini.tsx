c49f29d2fcc7c2ea010d06cdecf688bc935c6b13
Jnajera0430 <najerajoseantonio052@gmail.com>
2023-09-21 11:30:55 -0500

    fix: se arregló temas de semanticas

--- src/miniComponent/ListTech.mini.tsx
+++ src/miniComponent/ListTech.mini.tsx
@@ -3,7 +3,7 @@ import { ITech } from "../constants/tecnologies.constant";
 export const ListTechComponent = ({ name, src, description}: ITech) => (
     <li className="lg:w-1/3 mb-1 w-1/2 flex">
         <div>
-            <img src={src} alt={description} />
+            <img src={src} alt={description} className="w-full h-full"/>
         </div>
         <p className="text-gray-600 hover:text-gray-800">{name}</p>
     </li>
