cf6d1486c0c1f3bc091a3d59d4c14e707502816a
Jnajera0430 <najerajoseantonio052@gmail.com>
2023-09-28 11:52:52 -0500

    fix: se agregó cambios al para pantallas mobiles

--- src/miniComponent/Theme.miniMobile.tsx
+++ src/miniComponent/Theme.miniMobile.tsx
@@ -1,10 +1,10 @@
 import { UseContext } from "../api/hook/UseContext"
 import { Theme } from "../constants/theme.constante"
 
-export const ThemeIconMiniComponentMob = ({theme, ThemeIcon}:Theme) => {
-    const {darkMode} = UseContext()
+export const ThemeIconMiniComponentMob = ({ ThemeIcon}:Theme) => {
+    const {darkMode} = UseContext();
     return <div className='flex flex-row w-fit justify-center items-center group'>
-        <span className="hidden group-hover:block text-sm">{theme}</span> 
+        {/* <span className="hidden group-hover:block text-sm">{theme}</span>  */}
         <div className={`text-2xl ${darkMode ? "text-slate-200":"text-slate-700"} `}>
             <ThemeIcon/>
         </div>
